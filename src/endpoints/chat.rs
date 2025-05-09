d3756f3444b0ec3734092053c0b9221effc7a3c3
jacobhm98 <jacobhm98@gmail.com>
2025-05-09 20:48:46 +0200

    WIP

--- src/endpoints/chat.rs
+++ src/endpoints/chat.rs
@@ -1,30 +1,70 @@
+use axum::{
+    extract::{Json, State},
+    http::{self, header::HeaderMap}, response::IntoResponse,
+    http::StatusCode
+};
 use core::panic;
+use serde::{Deserialize, Serialize};
+use serde_json::Value;
+use std::sync::Arc;
 
-use axum::{extract::Json, http::header::HeaderMap};
+use crate::app_state::AppState;
 
-use serde::Deserialize;
-
-#[derive(Deserialize, Debug)]
+#[derive(Deserialize, Serialize, Debug)]
 pub struct CompletionRequest {
     pub model: String,
     pub messages: Vec<Message>,
 }
 
-#[derive(Deserialize, Debug)]
+#[derive(Deserialize, Serialize, Debug)]
 pub struct Message {
     pub role: String,
     pub content: String,
 }
 
-pub async fn chat_completions(headers: HeaderMap, Json(request_body): Json<CompletionRequest>) {
-    println!(
-        "model: {}, prompt: {}, role: {}",
-        request_body.model, request_body.messages[0].content, request_body.messages[0].role
-    );
-    
+
+
+pub async fn chat_completions(
+    State(state): State<Arc<AppState>>,
+    headers: HeaderMap,
+    Json(request_body): Json<CompletionRequest>,
+) {
+    let auth_token = extract_auth_token(&headers);
+    let response = send_request(state.http_client.clone(), auth_token, &request_body).await;
+    match response {
+        Ok(response) => into_response(response),
+        Err(error) => to_failed_response(error)
+    }
+}
+
+fn extract_auth_token(headers: &HeaderMap) -> &str {
+    // extract auth token
     let auth_token = match headers.get("Authorization") {
         Some(token) => token,
-        None => panic!("couldnt find auth token")
+        None => panic!("couldnt find auth token"),
     };
-    println!("Auth token: {}", auth_token.to_str().expect("auth_token was not a valid string"));
+    let auth_token = auth_token
+        .to_str()
+        .expect("auth_token was not a valid string");
+    auth_token
+}
+
+async fn send_request(
+    client: reqwest::Client,
+    auth_token: &str,
+    request_body: &CompletionRequest,
+) -> Result<reqwest::Response, reqwest::Error> {
+    let response = client
+        .post("https://api.openai.com/chat/completions")
+        .bearer_auth(auth_token)
+        .header("Content-Type", "application/json")
+        .json(&request_body)
+        .send()
+        .await?;
+    Ok(response)
+}
+
+async fn into_response(response: reqwest::Response) -> impl IntoResponse {
+    let response_body = response.json<Value>().await?;
+    (StatusCode::OK, response_body)
 }
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,18 @@
+mod app_state;
 mod endpoints;
+use app_state::AppState;
 use axum::{Router, routing::get, routing::post};
+use std::sync::Arc;
 
 #[tokio::main]
 async fn main() {
+    // initialize shared state
+    let shared_state = Arc::new(AppState::new());
+
     // build our application with a single route
     let app = Router::new()
         .route("/", get(|| async { "Hello, World!" }))
-        .route("/chat/completions", post(endpoints::chat::chat_completions));
+        .route("/chat/completions", post(endpoints::chat::chat_completions)).with_state(shared_state);
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:8000").await.unwrap();
     axum::serve(listener, app).await.unwrap();
