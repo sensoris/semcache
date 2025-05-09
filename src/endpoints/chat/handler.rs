2e9432f0255d23bfce0e45718aaeeab72b31614e
jacobhm98 <jacobhm98@gmail.com>
2025-05-09 14:51:31 +0200

    Call openai (#1)
    Add ci
    
    Fix ci
    
    Remove cargo version
    
    Fix fmt
    
    * handler func that parses request and headers
    
    * WIP
    
    * WIP
    
    * better error handling
    
    * Successful call to OpenAI
    
    * better error handling
    
    * reorganizing
    
    * cargo update
    
    * just pass whole AppState pointer
    
    * take proxy address from header
    
    * fix error messages

---
+++ src/endpoints/chat/handler.rs
@@ -0,0 +1,81 @@
+use axum::{
+    extract::{Json, State},
+    http::header::HeaderMap,
+};
+use std::sync::Arc;
+
+use crate::app_state::AppState;
+use url::Url;
+
+use super::{
+    dto::{CompletionRequest, CompletionResponse},
+    errors::CompletionError,
+};
+
+// CONSTANTS
+const PROXY_UPSTREAM_HEADER: &'static str = "X-LLM-PROXY-UPSTREAM";
+
+pub async fn completions(
+    State(state): State<Arc<AppState>>,
+    headers: HeaderMap,
+    Json(request_body): Json<CompletionRequest>,
+) -> Result<CompletionResponse, CompletionError> {
+    let auth_token = extract_auth_token(&headers)?;
+    let upstream_url = extract_proxy_upstream(&headers)?;
+    let reqwest_response = send_request(state, auth_token, upstream_url, request_body).await?;
+    let response = CompletionResponse::from_reqwest(reqwest_response).await?;
+    Ok(response)
+}
+
+fn extract_auth_token(headers: &HeaderMap) -> Result<&str, CompletionError> {
+    // extract auth token
+    headers
+        .get(axum::http::header::AUTHORIZATION)
+        .ok_or(CompletionError::InputValidation(String::from(
+            "Missing authorization header",
+        )))?
+        .to_str()
+        .map_err(|error| {
+            CompletionError::InputValidation(format!(
+                "authorization header could not be parsed as a string, {}",
+                error
+            ))
+        })
+}
+
+fn extract_proxy_upstream(headers: &HeaderMap) -> Result<Url, CompletionError> {
+    let raw = headers.get(PROXY_UPSTREAM_HEADER).ok_or_else(|| {
+        CompletionError::InputValidation(format!("Missing {} header", PROXY_UPSTREAM_HEADER))
+    })?;
+
+    let url_str = raw.to_str().map_err(|e| {
+        CompletionError::InputValidation(format!(
+            "{} header is not valid UTF-8: {}",
+            PROXY_UPSTREAM_HEADER, e
+        ))
+    })?;
+
+    Url::parse(url_str).map_err(|e| {
+        CompletionError::InputValidation(format!(
+            "{} header is not a valid URL: {}, error: {}",
+            PROXY_UPSTREAM_HEADER, url_str, e
+        ))
+    })
+}
+
+async fn send_request(
+    state: Arc<AppState>,
+    auth_token: &str,
+    upstream_url: Url,
+    request_body: CompletionRequest,
+) -> Result<reqwest::Response, reqwest::Error> {
+    let response = state
+        .http_client
+        .post(upstream_url)
+        .header(reqwest::header::AUTHORIZATION, auth_token)
+        .header(reqwest::header::CONTENT_TYPE, "application/json")
+        .json(&request_body)
+        .send()
+        .await?;
+    Ok(response)
+}
---
+++ src/endpoints/chat/mod.rs
@@ -0,0 +1,3 @@
+pub mod dto;
+pub mod errors;
+pub mod handler;
