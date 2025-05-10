867253d98b29466465b63cdf6679df52400808f0
jacobhm98 <jacobhm98@gmail.com>
2025-05-10 14:28:49 +0200

    better error handling

--- src/endpoints/chat.rs
+++ src/endpoints/chat.rs
@@ -1,7 +1,9 @@
 use axum::{
     extract::{Json, State},
-    http::header::HeaderMap, response::IntoResponse,
-    http::StatusCode
+    http::StatusCode,
+    http::header::HeaderMap,
+    response::IntoResponse,
+    response::Response,
 };
 use core::panic;
 use serde::{Deserialize, Serialize};
@@ -10,6 +12,9 @@ use std::sync::Arc;
 
 use crate::app_state::AppState;
 
+// DTO's
+
+// input type
 #[derive(Deserialize, Serialize, Debug)]
 pub struct CompletionRequest {
     pub model: String,
@@ -22,19 +27,84 @@ pub struct Message {
     pub content: String,
 }
 
+// Response type
+pub struct CompletionResponse {
+    body: Json<Value>,
+}
 
+impl CompletionResponse {
+    pub async fn from_reqwest(res: reqwest::Response) -> Result<Self, CompletionError> {
+        let status = res.status();
+        let text = res.text().await?;
+
+        // You can log it here
+        eprintln!("Upstream returned status {} with body:\n{}", status, text);
+        let parsed: serde_json::Value = serde_json::from_str(&text)?;
+        Ok(Self { body: Json(parsed) })
+    }
+}
+
+impl IntoResponse for CompletionResponse {
+    fn into_response(self) -> Response {
+        (StatusCode::OK, self.body).into_response()
+    }
+}
+
+// Error type
+pub enum CompletionError {
+    Upstream(reqwest::Error),
+    InvalidJson(serde_json::Error),
+}
+
+impl From<reqwest::Error> for CompletionError {
+    fn from(err: reqwest::Error) -> Self {
+        CompletionError::Upstream(err)
+    }
+}
+
+impl From<serde_json::Error> for CompletionError {
+    fn from(err: serde_json::Error) -> Self {
+        CompletionError::InvalidJson(err)
+    }
+}
+
+impl IntoResponse for CompletionError {
+    fn into_response(self) -> Response {
+        match self {
+            Self::Upstream(reqwest_err) => (
+                StatusCode::INTERNAL_SERVER_ERROR,
+                format!(
+                    "Error when calling upstream: {}, with status code: {}",
+                    reqwest_err.to_string(),
+                    reqwest_err
+                        .status()
+                        .get_or_insert(reqwest::StatusCode::INTERNAL_SERVER_ERROR)
+                ),
+            )
+                .into_response(),
+            Self::InvalidJson(serde_error) => {
+                eprintln!("error parsing json {}", serde_error);
+
+                (
+                    StatusCode::INTERNAL_SERVER_ERROR,
+                    "Failed to parse json from upstream",
+                )
+                    .into_response()
+            }
+        }
+    }
+}
 
 pub async fn chat_completions(
     State(state): State<Arc<AppState>>,
     headers: HeaderMap,
     Json(request_body): Json<CompletionRequest>,
-) {
+) -> Result<CompletionResponse, CompletionError> {
     let auth_token = extract_auth_token(&headers);
-    let response = send_request(state.http_client.clone(), auth_token, &request_body).await;
-    match response {
-        Ok(response) => into_response(response),
-        Err(error) => into_error_response(error),
-    }
+    let reqwest_response =
+        send_request(state.http_client.clone(), auth_token, &request_body).await?;
+    let response = CompletionResponse::from_reqwest(reqwest_response).await?;
+    Ok(response)
 }
 
 fn extract_auth_token(headers: &HeaderMap) -> &str {
@@ -55,22 +125,11 @@ async fn send_request(
     request_body: &CompletionRequest,
 ) -> Result<reqwest::Response, reqwest::Error> {
     let response = client
-        .post("https://api.openai.com/chat/completions")
+        .post("https://api.openai.com/v1/chat/completions")
         .bearer_auth(auth_token)
-        .header("Content-Type", "application/json")
+        .header(reqwest::header::CONTENT_TYPE, "application/json")
         .json(&request_body)
         .send()
         .await?;
     Ok(response)
-}
-
-async fn into_response(response: reqwest::Response) -> impl IntoResponse {
-     match response.json::<Value>().await {
-        Ok(response_body) => (StatusCode::OK, Json(response_body).into_response()),
-        Err(err) => into_error_response(err)
-     }
-}
-
-fn into_error_response<T: std::error::Error>(error: T) -> impl IntoResponse {
-    (StatusCode::INTERNAL_SERVER_ERROR, error.to_string())
 }
--- src/main.rs
+++ src/main.rs
@@ -12,7 +12,8 @@ async fn main() {
     // build our application with a single route
     let app = Router::new()
         .route("/", get(|| async { "Hello, World!" }))
-        .route("/chat/completions", post(endpoints::chat::chat_completions)).with_state(shared_state);
+        .route("/chat/completions", post(endpoints::chat::chat_completions))
+        .with_state(shared_state);
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:8000").await.unwrap();
     axum::serve(listener, app).await.unwrap();
