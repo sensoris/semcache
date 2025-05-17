e44719e837ee9157ae5983a8a7c8e0672aa5a996
Louis <me@louiscb.com>
2025-05-17 13:50:32 +0200

    Add dashboard and metrics

--- src/main.rs
+++ src/main.rs
@@ -3,13 +3,16 @@ mod cache;
 mod embedding;
 mod endpoints;
 mod utils;
-
+mod metrics;
 use app_state::AppState;
 use axum::{Router, routing::get, routing::post};
 use std::sync::Arc;
+use tower_http::services::ServeDir;
 
 #[tokio::main]
 async fn main() {
+    metrics::initialize_metrics_collection();
+
     let shared_state = Arc::new(AppState::new());
 
     let app = Router::new()
@@ -18,6 +21,12 @@ async fn main() {
             "/chat/completions",
             post(endpoints::chat::handler::completions),
         )
+        .route("/admin", get(endpoints::admin::handler::dashboard))
+        .route(
+            "/api/metrics",
+            get(endpoints::metrics::handler::get_metrics),
+        )
+        .nest_service("/static", ServeDir::new("assets"))
         .with_state(shared_state);
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:8000").await.unwrap();
---
+++ src/metrics.rs
@@ -0,0 +1,111 @@
+use chrono::{DateTime, Utc};
+use lazy_static::lazy_static;
+use prometheus::{IntCounter, register_int_counter};
+use serde::{Deserialize, Serialize};
+use serde_json::{self, to_string};
+use std::fs;
+use std::path::Path;
+use tokio::task;
+use tokio::time::{self, Duration};
+
+const METRICS_HISTORY_PATH: &str = "assets/metrics_history.json";
+
+// Note:
+// Saw online that we shouldn't be using lazy_static and instead use the new built in LazyLock
+// But I found it very complicated to understand how to use it and the prometheus examples
+// used lazy_static and works smoothly
+// https://github.com/tikv/rust-prometheus/blob/master/examples/example_int_metrics.rs
+// https://www.reddit.com/r/rust/comments/1iisfzg/lazycell_vs_lazylock_vs_oncecell_vs_oncelock_vs/
+lazy_static! {
+    pub static ref CHAT_COMPLETIONS: IntCounter =
+        register_int_counter!("incoming_requests", "Incoming Requests")
+            .expect("metric can be created");
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone, Copy)]
+#[serde(rename_all = "lowercase")]
+pub enum ChartType {
+    Bar,
+    Line,
+    Doughnut,
+}
+
+#[derive(Serialize, Deserialize, Clone)]
+pub struct Metrics {
+    pub name: String,
+    pub value: u64,
+    pub chart_type: ChartType,
+}
+
+#[derive(Serialize, Deserialize, Clone)]
+pub struct MetricsResponse {
+    pub timestamp: DateTime<Utc>,
+    pub metrics: Vec<Metrics>,
+}
+
+pub fn metrics() -> MetricsResponse {
+    let metrics = vec![Metrics {
+        name: "Chat completion requests".to_string(),
+        value: CHAT_COMPLETIONS.get(),
+        chart_type: ChartType::Line,
+    }];
+
+    MetricsResponse {
+        timestamp: Utc::now(),
+        metrics: metrics,
+    }
+}
+
+pub fn initialize_metrics_collection() {
+    if !Path::new(METRICS_HISTORY_PATH).exists() {
+        fs::write(METRICS_HISTORY_PATH, "[]").expect("Failed to create metrics history file");
+    }
+
+    // Start the background task for metrics collection
+    start_metrics_collection();
+}
+
+const MAX_LENGTH: usize = 50;
+
+fn start_metrics_collection() {
+    task::spawn(async move {
+        let mut interval = time::interval(Duration::from_secs(30));
+
+        loop {
+            interval.tick().await;
+            let current_metrics = metrics();
+
+            let mut history = history();
+
+            history.push(current_metrics);
+            history = limit_history_length(history);
+
+            // Write updated history back to the file
+            if let Ok(json_string) = serde_json::to_string_pretty(&history) {
+                let _ = fs::write(METRICS_HISTORY_PATH, json_string);
+            }
+        }
+    });
+}
+
+fn history() -> Vec<MetricsResponse> {
+    let history_content =
+        fs::read_to_string(METRICS_HISTORY_PATH).unwrap_or_else(|_| "[]".to_string());
+
+    let mut history: Vec<MetricsResponse> =
+        serde_json::from_str(&history_content).unwrap_or_else(|_| Vec::new());
+
+    history
+}
+
+fn limit_history_length(history: Vec<MetricsResponse>) -> Vec<MetricsResponse> {
+    if history.len() > MAX_LENGTH {
+        history
+            .clone()
+            .into_iter()
+            .skip(history.len() - MAX_LENGTH)
+            .collect()
+    } else {
+        history
+    }
+}
