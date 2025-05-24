8291966ba4f92bab843ae3738a71c8db9a968e9b
Jacob Hedén Malm <45039842+jacobhm98@users.noreply.github.com>
2025-05-24 18:28:36 +0200

    Cache: unit tests (#7)
    * Implemenet delete for semantic store
    
    * create a SemanticStore trait to facilitate mocking
    
    * use boxed embedding service to facilitate mocking
    
    * get_if_present test
    
    * tests for cache
    
    * cargo fmt
    
    * manually resolving merge conflict
    
    * cargo fmt
    
    ---------
    
    Co-authored-by: Louis <me@louiscb.com>

--- src/app_state.rs
+++ src/app_state.rs
@@ -1,6 +1,9 @@
-use crate::{cache::cache::Cache, embedding::fastembed::FastEmbedService};
+use crate::{
+    cache::{cache::Cache, semantic_store::flat_ip_faiss_store::FlatIPFaissStore},
+    embedding::fastembed::FastEmbedService,
+};
+use dashmap::DashMap;
 use reqwest::Client;
-use std::sync::Arc;
 
 pub struct AppState {
     pub http_client: Client,
@@ -9,9 +12,16 @@ pub struct AppState {
 
 impl AppState {
     pub fn new() -> Self {
+        let embedding_service = FastEmbedService::new();
+        let semantic_store = FlatIPFaissStore::new(embedding_service.get_dimensionality());
         Self {
             http_client: Client::new(),
-            cache: Cache::new(FastEmbedService::new(), 0.9),
+            cache: Cache::new(
+                Box::new(embedding_service),
+                Box::new(semantic_store),
+                DashMap::new(),
+                0.9,
+            ),
         }
     }
 }
---
+++ src/cache/semantic_store/mod.rs
@@ -0,0 +1,2 @@
+pub mod flat_ip_faiss_store;
+pub mod semantic_store;
