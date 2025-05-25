5c866a26211365c9d4f86d7ad89b5a9e53d56449
jacobhm98 <jacobhm98@gmail.com>
2025-05-25 15:02:15 +0200

    change println's to logs

--- src/endpoints/chat/handler.rs
+++ src/endpoints/chat/handler.rs
@@ -3,6 +3,7 @@ use axum::{
     http::header::HeaderMap,
 };
 use std::sync::Arc;
+use tracing::debug;
 
 use crate::app_state::AppState;
 use crate::metrics::CHAT_COMPLETIONS;
@@ -33,11 +34,11 @@ pub async fn completions(
 
     // return early if cache hit
     if let Some(saved_response) = state.cache.get_if_present(&prompt)? {
-        println!("CACHE HIT");
+        debug!("CACHE HIT");
         return Ok(CompletionResponse::from_cache(saved_response)?);
     };
 
-    println!("CACHE_MISS");
+    debug!("CACHE_MISS");
 
     // otherwise, send upstream request
     let auth_token = extract_auth_token(&headers)?;
--- src/main.rs
+++ src/main.rs
@@ -8,9 +8,16 @@ use app_state::AppState;
 use axum::{Router, routing::get, routing::post};
 use std::sync::Arc;
 use tower_http::services::ServeDir;
+use tracing::info;
+use tracing_subscriber::EnvFilter;
 
 #[tokio::main]
 async fn main() {
+    // TODO: this should come from config
+    tracing_subscriber::fmt()
+        .with_env_filter(EnvFilter::new("info"))
+        .init();
+
     metrics::initialize_metrics_collection();
 
     let shared_state = Arc::new(AppState::new());
@@ -29,6 +36,14 @@ async fn main() {
         .nest_service("/static", ServeDir::new("assets"))
         .with_state(shared_state);
 
-    let listener = tokio::net::TcpListener::bind("0.0.0.0:8000").await.unwrap();
+    let port = 8080;
+
+    let listener = tokio::net::TcpListener::bind(format!("0.0.0.0:{port}"))
+        .await
+        .unwrap();
+    info!(
+        "semcache-rs application started successfully on port {}",
+        port
+    );
     axum::serve(listener, app).await.unwrap();
 }
