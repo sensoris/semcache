5170a27e6d9517a21d2837032671f47991f13041
Louis <me@louiscb.com>
2025-05-26 14:07:46 +0200

    Add /

--- docs/semcache/docusaurus.config.ts
+++ docs/semcache/docusaurus.config.ts
@@ -13,7 +13,7 @@ const config: Config = {
   url: 'https://docs.semcache.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/semcache/',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
